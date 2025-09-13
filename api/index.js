6d020b1790669581f83ab66ef9bfc074acb4279e
Techligos <j85399461@gmail.com>
2025-09-13 18:02:28 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    return res.status(200).json({ message: "Hello from BTS Chatbot Backend 🚀" });
-  }
-  return res.status(405).json({ error: "Method not allowed" });
+  return res.status(200).json({ message: "Hello from BTS Chatbot Backend 🚀" });
 }
